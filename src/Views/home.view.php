130a843c2759897851468a08835886d6bab9a038
lamia 8512 <elf.lamia@hotmail.fr>
2026-03-27 11:50:27 +0100

    connexion

--- src/Views/home.view.php
+++ src/Views/home.view.php
@@ -1,6 +1,12 @@
 <?php
 require_once(__DIR__ . "/partials/head.view.php");
+if(isset($_SESSION['user'])){
+    ?>
+    <h1>Bonjour <?=$_SESSION['user']['pseudo']?></h1>
+    <?php
+}else{
 ?>
-<h1>Bienvenue ⋆.˚🦋༘⋆</h1>
+    <h1>Bonjour !</h1>
 <?php
+}
 require_once(__DIR__ . "/partials/footer.view.php");
